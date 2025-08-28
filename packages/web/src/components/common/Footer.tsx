45d7bd008d92da2060fb8bcdae09dd856a936ccc
Fernanda Oshiro <fernanda.sayuri@outlook.com>
2025-08-27 21:29:04 -0300

    feat: add footer component and enhance layout structure

---
+++ packages/web/src/components/common/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className='bg-background p-4 pt-8'>
+      <p className="text-center text-sm text-muted-foreground">
+        © 2025 Oshiro Imóveis. Todos os direitos reservados.
+      </p>
+    </footer>
+  )
+}
--- packages/web/src/components/lading-page/card.tsx
+++ packages/web/src/components/lading-page/card.tsx
@@ -1,8 +1,8 @@
 export function Card(props: {
   location: { address: string; title: string; description: string; min: string }
 }) {
-  return <div className="flex  bg-white rounded-md shadow-md sm:rounded-lg w-lg">
-    <div className="flex-1 bg-gray-500 min-h-max rounded-l-lg"></div>
+  return <div className="flex flex-col sm:flex-row bg-white rounded-md shadow-md sm:rounded-lg w-full">
+    <div className="flex-1 bg-gray-500 min-h-64 sm:min-h-max rounded-t-md sm:rounded-l-lg"></div>
     <div className="flex-1 p-8 flex flex-col gap-4">
       <p className="text-green font-bold">{props.location.title}</p>
       <p className="text-sm">{props.location.address}</p>
