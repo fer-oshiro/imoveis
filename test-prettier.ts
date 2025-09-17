2aaac5447ee626dd53632e73f248ff57e59b280a
Fernanda Oshiro <fernanda.sayuri@outlook.com>
2025-09-17 14:25:29 -0300

    fix: update Husky pre-commit hook to remove deprecation warning

--- test-prettier.ts
+++
@@ -1,9 +0,0 @@
-// Test file to verify Prettier formatting
-const testFunction = (param1: string, param2: number, param3: boolean) => {
-  if (param1 && param2 > 0 && param3) {
-    console.log('This should be formatted by Prettier')
-  }
-  return { param1, param2, param3 }
-}
-
-export default testFunction
