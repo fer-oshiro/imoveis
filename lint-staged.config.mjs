90a26a599574e700d4801ba3661a95e34b169f35
Fernanda Oshiro <fernanda.sayuri@outlook.com>
2025-10-18 17:16:34 -0300

    chore: remove .npmrc and update .prettierrc for singleQuote setting; standardize import statements in sst-env.d.ts files

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,4 @@
 export default {
-  'packages/**/*.{ts,tsx,js,jsx,mjs,cjs}': ['prettier -w'],
-  '*.{json,md,yml,yaml,css,scss,html}': ['prettier -w'],
+  'packages/**/*.{ts,tsx,js,jsx,mjs,cjs}': ['prettier -w --ignore-unknown'],
+  '*.{json,md,yml,yaml,css,scss,html}': ['prettier -w --ignore-unknown'],
 }
