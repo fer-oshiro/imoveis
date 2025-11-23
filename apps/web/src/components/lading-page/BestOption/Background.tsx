fcd9c0cd539365ff1acafa5e2e23c2fcec74fee4
Fernanda Oshiro <fernanda.sayuri@outlook.com>
2025-11-22 23:01:24 -0300

    feat: add Background component to BestOption for enhanced visual effects

---
+++ apps/web/src/components/lading-page/BestOption/Background.tsx
@@ -0,0 +1,40 @@
+'use client'
+import { AnimatePresence, motion, useAnimation, useInView } from 'motion/react'
+import { useEffect, useRef } from 'react'
+
+export const Background = () => {
+  const ref = useRef(null)
+  const isInView = useInView(ref, { amount: 0.3 })
+  const controls = useAnimation()
+
+  useEffect(() => {
+    if (isInView) controls.start('visible')
+  }, [isInView, controls])
+
+  return (
+    <div className="absolute top-0 left-0 -z-10 h-full w-full overflow-hidden bg-red-700" ref={ref}>
+      <AnimatePresence>
+        <motion.div
+          className="absolute top-12 left-0 h-22 w-44 bg-red-800 opacity-30"
+          variants={{
+            hidden: { left: '100%' },
+            visible: { left: '75%' },
+          }}
+          initial="hidden"
+          animate={controls}
+          transition={{ duration: 1.5, ease: 'easeOut' }}
+        />
+        <motion.div
+          className="absolute top-52 left-0 h-44 w-4xl bg-red-800 opacity-30"
+          variants={{
+            hidden: { left: '100%' },
+            visible: { left: '50%' },
+          }}
+          initial="hidden"
+          animate={controls}
+          transition={{ duration: 3.5, ease: 'easeOut', delay: 1 }}
+        />
+      </AnimatePresence>
+    </div>
+  )
+}
--- apps/web/src/components/lading-page/BestOption/index.tsx
+++ apps/web/src/components/lading-page/BestOption/index.tsx
@@ -1,17 +1,28 @@
 import { Suspense } from 'react'
+
+import { Background } from './Background'
 import { Divider } from './Divider'
 
 export const BestOption = () => {
   return (
-    <section className="relative mt-24 h-screen bg-red-300">
+    <section className="relative mt-24 flex py-24">
       <Suspense fallback={<div className="h-24" />}>
         <Divider />
       </Suspense>
-      <div>
-        <h1>Best Option Component</h1>
-      </div>
-      <div>
-        <span>Melhor Avaliado</span>
+      <Suspense
+        fallback={
+          <div className="absolute top-0 left-0 -z-10 h-full w-full overflow-hidden bg-red-700" />
+        }
+      >
+        <Background />
+      </Suspense>
+      <div className="z-20 mt-12 px-6 text-white">
+        <h2 className="text-sm font-medium">A UM PASSO DE VOCÊ</h2>
+        <h1 className="my-4 text-2xl font-bold">Pertinho do Metrô - São Paulo</h1>
+        <div>
+          Conheça nossos empreendimentos prontos para morar, com design em localizações
+          estratégicas, próximas ao metrô, comércios e o melhor custo-benefício de São Paulo.
+        </div>
       </div>
     </section>
   )
