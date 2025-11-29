5029b6eb55f6a4134eda41d33bf6aeb06cfd1717
Fer Oshiro <fernanda.sayuri@outlook.com>
2025-11-29 12:18:14 -0300

    feat: enhance Review component with improved styling and interactivity

--- apps/web/src/components/lading-page/Review.tsx
+++ apps/web/src/components/lading-page/Review.tsx
@@ -6,18 +6,36 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '../ui/carousel'
+import Link from 'next/link'
+import { Star } from 'lucide-react'
 
 export const Review = () => {
   return (
-    <div>
+    <div className='px-4 sm:px-16'>
       <Carousel>
         <CarouselContent>
           {Array.from({ length: 5 }).map((_, index) => (
             <CarouselItem key={index} className="sm:basis-1/2 lg:basis-1/3">
-              <div className="rounded-lg bg-gray-100 p-4">
+              <div className="rounded-sm bg-gray-100 p-4 border text-gray-600 text-sm">
+                <p>
                 Um anfitrião muito bom, sempre atento a qualquer necessidade que eu tivesse. O lugar
                 é muito agradável, limpo e tem tudo o que você precisa. É a melhor opção para uma
                 estadia em São Paulo.
+                </p>
+                <div className='flex justify-between items-center mt-16'>
+                  <Link href="airbnb.com/h/agostinhogomesap2" className='font-bold text-red-700'>Ver avaliação</Link>
+                  <div className='flex gap-2 items-center'>
+                    <p>(5.0)</p>
+                    <div className='flex gap-1'>
+                      {
+                        Array.from({ length: 5 }).map((_, starIndex) => (
+                          <Star key={starIndex} className='text-yellow-500' fill='var(--color-yellow-500)' height={12} width={12} />
+                        ))
+                      }
+                    </div>
+                  </div>
+                </div>
+                <p></p>
               </div>
             </CarouselItem>
           ))}
